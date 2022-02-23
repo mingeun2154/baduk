e926224fb32e42160f27f94f72652a31a5f15a76
mingeun <mindal99546@gmail.com>
2022-02-23 10:11:12 +0900

    initial commit

---
+++ src/main/java/BadukApplication.java
@@ -0,0 +1,8 @@
+import frame.*;
+
+public class BadukApplication {
+    public static void main(String[] args){
+        System.out.println("start game");
+        BadukFrame mainframe = BadukFrame.getFrame();
+    }
+}
