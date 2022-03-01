cb40c73ffffdf7df748c1e61816e10ade459187a
Mingeun Park <mindal99546@gmail.com>
2022-03-01 21:28:23 +0900

    feature:Implement StartScreenController

--- src/main/java/controller/StartScreenController.java
+++ src/main/java/controller/StartScreenController.java
@@ -1,6 +1,23 @@
 package controller;
 
+import domain.Button;
 import renderer.BRenderer;
 
-public class StartScreenController {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartScreenController implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button=(JButton) e.getSource();
+        if(button.getName()=="LoadGameButton"){
+            System.out.println("불러오기");
+        }else if(button.getName()=="NewGameButton"){
+            System.out.println("새 게임");
+        }else{
+            System.out.println("nothing");
+        }
+    }
 }
