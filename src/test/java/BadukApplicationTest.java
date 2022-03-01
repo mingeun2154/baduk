f6fce5aa5dd5176f0e09b212a4775a05387e0bb0
Mingeun Park <mindal99546@gmail.com>
2022-03-01 23:18:31 +0900

    refactor:No need to test yet

--- src/test/java/BadukApplicationTest.java
+++ src/test/java/BadukApplicationTest.java
@@ -1,9 +1,5 @@
-import performer.BPerformer;
-import performer.BPerformerTest;
 
 public class BadukApplicationTest {
         public static void main(String[] args){
-                BPerformerTest bPerformerTest = new BPerformerTest();
-                bPerformerTest.시작화면_출력();
         }
 }
